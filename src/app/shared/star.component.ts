9da0ad9658076e8a994ed55e17471cf6bc13e7e3
Ronique Young <youngronique09@gmail.com>
2018-08-31 22:59:46 -0500

    fixing ratings

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
     selector: 'pm-start',
@@ -8,7 +8,7 @@ import { Component, OnChanges } from '@angular/core';
 })
 
 export class StarComponent implements OnChanges {
-    rating: number = 4;
+   @Input() rating: number;
     starWidth: number;
 
 
